a71c752a058950f09f5b96aab2f5a5392204b45c
Nikunj Mandani <nikunj.mandani@gmail.com>
2021-03-12 13:27:23 -0500

    initilal set up for the property WebAPI

---
+++ Controllers/CityController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PropertyWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "atlanta", "new york", "dayton" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "atlanta";
+        }
+    }
+}
