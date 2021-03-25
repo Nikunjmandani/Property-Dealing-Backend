5e40f5483fe6f014beaed7da06279fb22c90e2c9
Nikunj Mandani <nikunj.mandani@gmail.com>
2021-03-25 15:16:52 -0400

    Implement async add and delete operation in city

--- Controllers/CityController.cs
+++ Controllers/CityController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PropertyWebAPI.Data;
+using PropertyWebAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,13 +21,50 @@ namespace PropertyWebAPI.Controllers
         {
             this.dc = dc;
         }
-        
+        //GET api city
         [HttpGet]
-        public IActionResult GetCities()
+        public async Task<IActionResult> GetCities()
         {
-            var cities = dc.Cities.ToList();
+            var cities = await dc.Cities.ToListAsync();
             return Ok(cities);
         }
 
+        // Post api/city/add?cityname=Miami
+        // Post api/city/add/Los Angeles
+        [HttpPost("add")]
+        [HttpPost("add/{cityname}")]
+        public async Task<IActionResult> AddCity(string cityName)
+        {
+            City city = new City();
+            city.Name = cityName;
+            await dc.Cities.AddAsync(city);
+            await dc.SaveChangesAsync();
+            return Ok(city);
+        }
+
+        // Post api/city/post --Post the data in JSON Format
+        [HttpPost("post")]
+        public async Task<IActionResult> AddCity(City city)
+        {
+            // City city = new City();
+            // city.Name=cityName;
+            await dc.Cities.AddAsync(city);
+            await dc.SaveChangesAsync();
+            return Ok(city);
+        }
+
+
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> DeleteCity(int id)
+        {
+            var city = await dc.Cities.FindAsync(id);
+
+            if (city == null)
+                return NotFound();
+
+            dc.Cities.Remove(city);
+            await dc.SaveChangesAsync();
+            return Ok(id);
+        }
     }
 }
